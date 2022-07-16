0ac0a7d03102cb95ef6b338fd26c2d2f477e934b
Osamuyi <muyixone@yahoo.com>
2022-07-16 23:15:39 +0100

    first intro to JS

--- calc.js
+++ calc.js
@@ -1,2 +1,17 @@
-let displayScreen = document.getElementById("display");
+function printSymbol(e) {
+    let displayScreen = document.getElementById("display");
+    let object = e.target;
+
+    displayScreen.value += object.textContent
+}
+
+
+let displayScreen = document.getElementById("display").value;
+
+let numbers = document.getElementsByClassName("btn_white");
+
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].value.addEventListener("click", printSymbol);
+    }
+
 
